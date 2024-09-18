68983be55af02b2641b215b878981dee25de3bbd
Jumjumz <159726948+Jumjumz@users.noreply.github.com>
2024-09-18 19:04:14 +0800

    add env file

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send("Server is ready");
+});
+
 app.listen(6969, () => {
     console.log("Server started at http://localhost:6969");
 });
