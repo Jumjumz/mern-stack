533758fe50c5ba88c866c33c7ee679f23470c5c6
Jumjumz <159726948+Jumjumz@users.noreply.github.com>
2024-09-18 19:17:42 +0800

    connect to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
-import express from 'express';
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
 
+dotenv.config();
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Server is ready");
+    
 });
 
+
 app.listen(6969, () => {
+    connectDB();
     console.log("Server started at http://localhost:6969");
 });
